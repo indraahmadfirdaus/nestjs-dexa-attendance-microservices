d9f7eb2afcf757bc0b499b9f6a20ed1404e6a012
Indra <indraahmadfirdaus@gmail.com>
2025-10-02 22:37:52 +0700

    feat: init common modules 1. decorators 2. dto 3. events 4. filters 5. guards 6. interceptors 7. utils

--- apps/gateway/src/gateway.controller.ts
+++ apps/gateway/src/gateway.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { GatewayService } from './gateway.service';
+import { TransformInterceptor } from '@libs/common';
 
 @Controller()
+@UseInterceptors(TransformInterceptor)
 export class GatewayController {
   constructor(private readonly gatewayService: GatewayService) {}
 
